1cad1e88e99a73e88fd67b32c453b8293f520cfd
Kazumean <kazumin.angelav@gmail.com>
2023-01-26 11:04:19 +0900

    「求人を作成する」ボタンを押下後にバリデーションが働いた際、もともと入力していた値を入力欄に保持する機能の実装。

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -10,7 +10,7 @@
             @csrf
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">会社名</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" value="{{ old('company') }}"/>
 
                 @error('company')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -19,8 +19,7 @@
 
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">職種名</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
-                    placeholder="例: Laravel開発中級者" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title" placeholder="例: Laravel開発中級者" value="{{ old('title') }}" />
                 
                 @error('title')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -29,8 +28,7 @@
 
             <div class="mb-6">
                 <label for="location" class="inline-block text-lg mb-2">勤務地</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-                    placeholder="例: 神奈川県鎌倉市（リモート勤務あり） など" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location" placeholder="例: 神奈川県鎌倉市（リモート勤務あり） など" value="{{ old('location') }}" />
                 
                 @error('location')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -39,7 +37,7 @@
 
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">連絡先メールアドレス</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{ old('email') }}" />
 
                 @error('email')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -48,7 +46,7 @@
 
             <div class="mb-6">
                 <label for="website" class="inline-block text-lg mb-2">会社ホームページ</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" value="{{ old('website') }}" />
 
                 @error('website')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -57,8 +55,7 @@
 
             <div class="mb-6">
                 <label for="tags" class="inline-block text-lg mb-2">タグ (半角カンマで区切ってください)</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-                    placeholder="例: Laravel, バックエンド, Postgres など" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags" placeholder="例: Laravel, バックエンド, Postgres など" value="{{ old('tags') }}"/>
                 
                 @error('tags')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -72,7 +69,7 @@
 
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">お仕事詳細</label>
-                <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10" placeholder="具体的な業務内容やご要望、給与等を記入してください。"></textarea>
+                <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10" placeholder="具体的な業務内容やご要望、給与等を記入してください。">{{ old('description') }}</textarea>
 
             @error('description')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
