51e9b1faf77626bf9dd692c93b111044fc11ab5d
Kazumean <kazumin.angelav@gmail.com>
2023-01-30 17:30:25 +0900

    ログイン時にユーザー情報を画面上に表示する処理の実装・ログイン時のみ求人情報を管理するリンクを表示させる処理の実装。

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,13 +29,26 @@
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src={{ asset('images/logo.png') }} alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
+            @auth
+            <li>
+                <span class="font-bold uppercase">
+                    {{ auth()->user()->name}}さん、ようこそ！
+                </span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel"><i
+                        class="fa-solid fa-gear"></i>求人を管理する</a>
+            </li>
+            @else
             <li>
                 <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> 会員登録</a>
             </li>
             <li>
                 <a href="/login" class="hover:text-laravel"><i
                         class="fa-solid fa-arrow-right-to-bracket"></i>ログイン</a>
-            </li>
+            </li>               
+            @endauth
+
         </ul>
     </nav>
 
